523fab0823da390d9e034c3ad10a3c5f46a1ee67
Hosamov <hosamov@hotmail.com>
2021-12-28 12:53:51 -0800

    add dependencies and dev/prod

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const { app, BrowserWindow } = require('electron');
 
+// Set env: 'development' or 'production'
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform === 'darwin' ? true: false;
+
 let mainWindow;
 
 function createMainWindow () {
@@ -7,11 +13,27 @@ function createMainWindow () {
   mainWindow = new BrowserWindow({
     title: 'ImageShrink',
     width: 500,
-    height: 600
+    height: 600,
+    icon: `./${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev, // Allow resizing window in development only
   });
 
-  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  mainWindow.loadFile('./app/index.html');
+
 }
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
 
 // Run on event: ready, calling createMainWindow()
 app.on('ready', createMainWindow);
